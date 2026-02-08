8ac7508c9f2230bd10484887f430deb9937e16cb
radar roark <122068506+radarroark@users.noreply.github.com>
2026-02-08 08:56:25 -0500

    rename zig file

--- build.zig
+++ build.zig
@@ -11,7 +11,7 @@ pub fn build(b: *std.Build) void {
     const exe = b.addExecutable(.{
         .name = "xitlog",
         .root_module = b.createModule(.{
-            .root_source_file = b.path("src/main.zig"),
+            .root_source_file = b.path("src/lib.zig"),
             .target = target,
             .optimize = .ReleaseSmall,
         }),
